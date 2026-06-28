9fe691b0d916bc2b15f9dd3ff32ab3bd8ef6db65
rivalscott04 <rival.biasrori@outlook.com>
2026-06-28 21:41:20 +0800

    Update PDF preview modal to prevent overflow issues
    - Added 'overflow-hidden' class to the modal for better content management.
    - Ensured the layout remains responsive while accommodating dynamic content.

--- resources/views/filament/reports/modals/pdf-preview.blade.php
+++ resources/views/filament/reports/modals/pdf-preview.blade.php
@@ -69,7 +69,7 @@
         },
     }"
     x-init="init()"
-    class="-mx-6 -mb-6 flex min-h-0 flex-1 flex-col"
+    class="-mx-6 -mb-6 flex min-h-0 flex-1 flex-col overflow-hidden"
 >
     <div
         x-show="loading"
